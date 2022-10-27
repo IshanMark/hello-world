f11c1e7489062903a845dcd9be8505b7740d867b
IshanMark <rupasing-cs18036@stu.kln.ac.lk>
2022-10-27 05:31:53 +0530

    Functional Components ii

--- src/components/Greet.js
+++ src/components/Greet.js
@@ -1,7 +1,9 @@
 import React from "react"
 
-function Greet() {
-    return <h1>Hello Imashi</h1>
-}
+//function Greet() {
+//    return <h1>Hello Imashi</h1>
+//}
+
+const Greet = () => <h1>Hello Imashi</h1>
 
 export default Greet;
