701435ee1331cd39965daf12936ec406e048e36b
IshanMark <rupasing-cs18036@stu.kln.ac.lk>
2022-10-30 06:21:30 +0530

    edit jsx without

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,10 +1,24 @@
 import React from "react";
 
 const Hello = () => {
-    return (
-        <div>
-            <h1>Hello Gamage</h1>
-        </div>
+
+    // with JSX Command
+//    return (
+//        <div>
+//            <h1>Hello Gamage</h1>
+//        </div>
+//    )
+
+    // return React.createElement('div',null,<h1>Hello Gamage</h1>)
+
+    // return React.createElement('div', null, 'h1', 'Hello Gamage')
+
+    // return React.createElement('div' ,null ,React.createElement('h1',null,'Hello Gamage') )
+
+    return React.createElement(
+        'div',
+        {id: 'hello' ,className:"jsxSampleClass"},
+        React.createElement('h1', null, 'Hello Gamage')
     )
 }
 
