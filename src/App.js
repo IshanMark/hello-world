1ef23c5c980d2db5af27d018f060e4e1ecaa1ac4
IshanMark <rupasing-cs18036@stu.kln.ac.lk>
2022-10-31 05:17:01 +0530

    Add props

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
 import React from "react"
 //import logo from './logo.svg';
 import './App.css';
-import {Greet} from './components/Greet'
+import Greet from './components/Greet'
 import Welcome from "./components/Welcome";
-import Hello from "./components/Hello"
+//import Hello from "./components/Hello"
 
 function App() {
   return (
     <div className="App">
-      <Greet/>
+      {/*<Greet/>*/}
 
-      <Welcome/>
+      {/*<Welcome/>*/}
 
-      <Hello/>
+      {/*<Hello/>*/}
+
+      <Greet name = "Imashi" heroname = "BatWomen">
+        <p>This is children props</p>
+      </Greet>
+      <Greet name = "Anjana" heroname = "SuperWomen">
+        <button>Action</button>
+      </Greet>
+      <Greet name = "Gamage" heroname = "Wonder Women"/>
+
+      <Welcome name = "Imashi" heroname = "BatWomen"></Welcome>
+      <Welcome name = "Anjana" heroname = "SuperWomen"></Welcome>
+      <Welcome name = "Gamage" heroname = "Wonder Women"></Welcome>
+
     </div>
   );
 }
--- src/components/Greet.js
+++ src/components/Greet.js
@@ -4,6 +4,16 @@ import React from "react"
 //    return <h1>Hello Imashi</h1>
 //}.
 
-export const Greet = () => <h1>Hello Imashi</h1>
+const Greet = props => {
+    console.log(props)
+    return (
+        <div>
+            <h1>Hello {props.name} c.s {props.heroname}</h1>
+            {props.children}
+        </div>
+        
+    )
+    //return<h1>Hello Imashi</h1>
+}
 
-//export default Greet;
+export default Greet
